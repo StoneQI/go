4ce776492394591d0778b83bf71ce9e18a5ddb25
stoneyang <xiyaozhe@gmail.com>
2021-03-03 13:54:58 +0000

    note-sync syncing

--- learn/asm/main.go
+++ learn/asm/main.go
@@ -17,13 +17,13 @@ func main() {
 
 0x001d 00029 (/workspaces/go/learn/asm/main.go:10)      XORPS   X0, X0
 0x0020 00032 (/workspaces/go/learn/asm/main.go:10)      MOVUPS  X0, "".w+32(SP)
-0x0025 00037 (/workspaces/go/learn/asm/main.go:12)      LEAQ    go.string."1234"(SB), AX
-0x002c 00044 (/workspaces/go/learn/asm/main.go:12)      MOVQ    AX, (SP)
-0x0030 00048 (/workspaces/go/learn/asm/main.go:12)      MOVQ    $4, 8(SP)
-0x0039 00057 (/workspaces/go/learn/asm/main.go:12)      PCDATA  $1, $0
-0x0039 00057 (/workspaces/go/learn/asm/main.go:12)      CALL    strings.NewReader(SB)
-0x003e 00062 (/workspaces/go/learn/asm/main.go:12)      MOVQ    16(SP), AX
-0x0043 00067 (/workspaces/go/learn/asm/main.go:12)      MOVQ    AX, ""..autotmp_1+24(SP)
-0x0048 00072 (/workspaces/go/learn/asm/main.go:12)      LEAQ    go.itab.*strings.Reader,io.Reader(SB), CX
-0x004f 00079 (/workspaces/go/learn/asm/main.go:12)      MOVQ    CX, "".w+32(SP)
-0x0054 00084 (/workspaces/go/learn/asm/main.go:12)      MOVQ    AX, "".w+40(SP)
+0x0025 00037       
+0x002c 00044       
+0x0030 00048       
+0x0039 00057       
+0x0039 00057       
+0x003e 00062       
+0x0043 00067       
+0x0048 00072       
+0x004f 00079       
+0x0054 00084       
