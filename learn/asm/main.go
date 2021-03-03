e8a979dbf567129cb6243a7ab85c1c0cfbfaead2
stoneyang <xiyaozhe@gmail.com>
2021-03-03 13:56:56 +0000

    note-sync syncing

--- learn/asm/main.go
+++ learn/asm/main.go
@@ -10,21 +10,11 @@ func main() {
 	var w io.Reader
 
 	var a *strings.Reader
+
+	w = a
+
 	w = strings.NewReader("1234")
 
 	_ = w
 
 }
-
-0x001d 00029 (/workspaces/go/learn/asm/main.go:10)      XORPS   X0, X0
-0x0020 00032 (/workspaces/go/learn/asm/main.go:10)      MOVUPS  X0, "".w+32(SP)
-0x0025 00037       
-0x002c 00044       
-0x0030 00048       
-0x0039 00057       
-0x0039 00057       
-0x003e 00062       
-0x0043 00067       
-0x0048 00072       
-0x004f 00079       
-0x0054 00084       
