56cb73611991b707e74c1a10d442a30dde97085d
stoneyang <xiyaozhe@gmail.com>
2021-03-03 13:57:25 +0000

    note-sync syncing

--- learn/asm/main.go
+++ learn/asm/main.go
@@ -18,3 +18,9 @@ func main() {
 	_ = w
 
 }
+
+0x0025 00037 (/workspaces/go/learn/asm/main.go:12)      MOVQ    $0, "".a+24(SP)
+0x002e 00046 (/workspaces/go/learn/asm/main.go:14)      MOVQ    $0, ""..autotmp_2+32(SP)
+0x0037 00055 (/workspaces/go/learn/asm/main.go:14)      LEAQ    go.itab.*strings.Reader,io.Reader(SB), AX
+0x003e 00062 (/workspaces/go/learn/asm/main.go:14)      MOVQ    AX, "".w+40(SP)
+0x0043 00067 (/workspaces/go/learn/asm/main.go:14)      MOVQ    $0, "".w+48(SP)
