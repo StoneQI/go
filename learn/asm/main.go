5b19be311256766f9322fcd8ac2526e885e0841c
stoneyang <xiyaozhe@gmail.com>
2021-03-03 13:38:33 +0000

    note-sync syncing

--- learn/asm/main.go
+++ learn/asm/main.go
@@ -15,3 +15,12 @@ func main() {
 	}
 
 }
+
+MOVQ    $0, "".w+56(SP)
+        0x0038 00056 (/workspaces/go/learn/asm/main.go:8)       LEAQ    type.chan int(SB), AX
+        0x003f 00063 (/workspaces/go/learn/asm/main.go:8)       MOVQ    AX, (SP)
+        0x0043 00067 (/workspaces/go/learn/asm/main.go:8)       MOVQ    $0, 8(SP)
+        0x004c 00076 (/workspaces/go/learn/asm/main.go:8)       PCDATA  $1, $1
+        0x004c 00076 (/workspaces/go/learn/asm/main.go:8)       CALL    runtime.makechan(SB)
+        0x0051 00081 (/workspaces/go/learn/asm/main.go:8)       MOVQ    16(SP), AX
+        0x0056 00086 (/workspaces/go/learn/asm/main.go:8)       MOVQ    AX, "".w1+48(SP)
