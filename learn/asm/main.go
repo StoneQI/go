7f4b8b317d3d52b5c121bb4f5dfcf8c999b759e1
stoneyang <xiyaozhe@gmail.com>
2021-03-04 02:20:47 +0000

    note-sync syncing

--- learn/asm/main.go
+++ learn/asm/main.go
@@ -8,24 +8,22 @@ func main() {
 		Sum() int
 	}
 
-	var t *int       // nil of type *tree
-	var s Summer = t // nil指针，可以是合法的interface类型的值
-	// 此时，对接接口类型变量s而言，其类型为*tree，值为nil，也就是说(*tree, nil)行的interface
+	
 
-	fmt.Println(t == nil, s.Sum()) // true, 0
+	
 
-	// type ints []int
-	// func (i *ints) Sum() int {
-	// 	s := 0
-	// 	for _, v := range i{
-	// 	  s += v
-	// 	}
+	type ints []int
+	func (i *ints) Sum() int {
+		s := 0
+		for _, v := range i{
+		  s += v
+		}
 
-	// 	return s
-	//   }
+		return s
+	  }
 
-	// var i ints
-	// var s Sumer = i // nil value can satisfy interface
-	// fmt.Println(s==nil, s.Sum()) // true, 0
+	var i ints
+	var s Sumer = i // nil value can satisfy interface
+	fmt.Println(s==nil, s.Sum()) // true, 0
 
 }
